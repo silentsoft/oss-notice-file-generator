d6c8ae8ee245964bbc026ff3904de50301a57fb7
silentsoft <hyesung.lee@silentsoft.org>
2020-05-04 02:09:21 +0900

    Add test cases for new licenses

--- src/test/java/org/silentsoft/oss/LicenseTest.java
+++ src/test/java/org/silentsoft/oss/LicenseTest.java
@@ -8,12 +8,17 @@ import java.io.Reader;
 import org.junit.Assert;
 import org.junit.Test;
 import org.silentsoft.oss.license.ApacheLicense2;
+import org.silentsoft.oss.license.ApacheLicense2_0;
 import org.silentsoft.oss.license.BCLLicense;
 import org.silentsoft.oss.license.BSD2ClauseLicense;
 import org.silentsoft.oss.license.BSD3ClauseLicense;
+import org.silentsoft.oss.license.GPL2_0License;
+import org.silentsoft.oss.license.GPL3_0License;
 import org.silentsoft.oss.license.ICULicense;
 import org.silentsoft.oss.license.JSONLicense;
+import org.silentsoft.oss.license.LGPL2_1License;
 import org.silentsoft.oss.license.LGPL3License;
+import org.silentsoft.oss.license.LGPL3_0License;
 import org.silentsoft.oss.license.MITLicense;
 
 public class LicenseTest {
@@ -21,6 +26,11 @@ public class LicenseTest {
 	@Test
 	public void ApacheLicense2Test() throws IOException {
 		Assert.assertEquals(read("/license/Apache License 2.0.txt"), new ApacheLicense2().getContent());
+	}
+	
+	@Test
+	public void ApacheLicense2_0Test() throws IOException {
+		Assert.assertEquals(read("/license/Apache License 2.0.txt"), new ApacheLicense2_0().getContent());
 	}
 	
 	@Test
@@ -39,6 +49,16 @@ public class LicenseTest {
 	}
 	
 	@Test
+	public void GPL2_0LicenseTest() throws IOException {
+		Assert.assertEquals(read("/license/GPL-2.0.txt"), new GPL2_0License().getContent());
+	}
+	
+	@Test
+	public void GPL3_0LicenseTest() throws IOException {
+		Assert.assertEquals(read("/license/GPL-3.0.txt"), new GPL3_0License().getContent());
+	}
+	
+	@Test
 	public void ICULicenseTest() throws IOException {
 		Assert.assertEquals(read("/license/ICU License.txt"), new ICULicense().getContent());
 	}
@@ -46,6 +66,16 @@ public class LicenseTest {
 	@Test
 	public void JSONLicenseTest() throws IOException {
 		Assert.assertEquals(read("/license/JSON License.txt"), new JSONLicense().getContent());
+	}
+	
+	@Test
+	public void LGPL2_1LicenseTest() throws IOException {
+		Assert.assertEquals(read("/license/LGPL-2.1.txt"), new LGPL2_1License().getContent());
+	}
+	
+	@Test
+	public void LGPL3_0LicenseTest() throws IOException {
+		Assert.assertEquals(read("/license/LGPL-3.0.txt"), new LGPL3_0License().getContent());
 	}
 	
 	@Test
